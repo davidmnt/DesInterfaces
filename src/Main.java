58e16cdeb2a1c7b32e3853b9f39176b36dbcee0d
david <david.montejano@hotmail.com>
2023-09-19 21:39:46 +0200

    interfaz

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        JFrame primeraVentana = new JFrame();
+        JPanel panelPrincipal = new JPanel();
+
+        primeraVentana.setBounds(10,10,300,200);
+        primeraVentana.setVisible(true);
+        primeraVentana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+
+
+
+        panelPrincipal.add(new JLabel("Usuario:"),
+        new GridBagConstraints(0,0,1,1,1.0,1.0,GridBagConstraints.CENTER,GridBagConstraints.BOTH,
+        new Insets(0,0,0,0),0,0));
+
+        panelPrincipal.add(new JTextField(20));
+
+       panelPrincipal.add(
+                new JButton("Enviar"),
+                        new GridBagConstraints(1,2,2,1,1.0,1.0,GridBagConstraints.SOUTH,GridBagConstraints.HORIZONTAL,
+                                new Insets(0,0,0,0),0,0));
+      /*  panelPrincipal.add(
+                new JButton("BOTON 2"),
+                new GridBagConstraints(1,0,1,1,1.0,1.0,GridBagConstraints.CENTER,GridBagConstraints.BOTH,
+                        new Insets(0,0,0,0),0,0));
+        panelPrincipal.add(
+                new JButton("BOTON 3"),
+                new GridBagConstraints(2,0,1,1,1.0,1.0,GridBagConstraints.CENTER,GridBagConstraints.BOTH,
+                        new Insets(0,0,0,0),0,0));*/
+
+        primeraVentana.setContentPane(panelPrincipal);
+
+
+
+
+
     }
+
 }
---
+++ src/Primero.java
@@ -0,0 +1,142 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Primero extends javax.swing.JFrame{
+
+    public static void main(String[] args) {
+
+
+        JFrame frame = new JFrame("Calculadora");
+        frame.setContentPane(new Primero().calculadora);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+
+    }
+
+    private String cadenaNum;
+    private String operacion;
+    private double pirmerNum;
+    private boolean activado = true;
+    private boolean punto = true;
+    private JPanel calculadora;
+    private JTextField Resultado;
+    private JButton a4Button;
+    private JButton a8Button;
+    private JButton a5Button;
+    private JButton a6Button;
+    private JButton a9Button;
+    private JButton a2Button ;
+    private JButton a7Button;
+    private JButton a1Button;
+    private JButton a3Button;
+    private JButton a0Button ;
+    private JButton sumar;
+    private JButton restar;
+    private JButton dividir;
+    private JButton multiplicar ;
+    private JButton igual;
+    private  JLabel Titulo ;
+    private JButton Borrar;
+
+
+    public Primero() {
+
+
+        a1Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Resultado.setText(Resultado.getText() + a1Button.getText());
+            }
+        });
+        a2Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Resultado.setText(Resultado.getText() + a2Button.getText());
+            }
+        });
+        a3Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Resultado.setText(Resultado.getText() + a3Button.getText());
+            }
+        });
+        a4Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Resultado.setText(Resultado.getText() + a4Button.getText());
+            }
+        });
+        a5Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Resultado.setText(Resultado.getText() + a5Button.getText());
+            }
+        });
+        a6Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Resultado.setText(Resultado.getText() + a6Button.getText());
+            }
+        });
+        a7Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Resultado.setText(Resultado.getText() + a7Button.getText());
+            }
+        });
+        a8Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Resultado.setText(Resultado.getText() + a8Button.getText());
+            }
+        });
+        a9Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Resultado.setText(Resultado.getText() + a9Button.getText());
+            }
+        });
+        Borrar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Resultado.setText("");
+            }
+        });
+        sumar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Resultado.setText(Resultado.getText() + "+");
+            }
+        });
+        restar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Resultado.setText(Resultado.getText() + "-");
+            }
+        });
+        multiplicar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Resultado.setText(Resultado.getText() + "*");
+            }
+        });
+        dividir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Resultado.setText(Resultado.getText() + "/");
+            }
+        });
+    }
+
+    private void createUIComponents() {
+
+
+
+    }
+}
